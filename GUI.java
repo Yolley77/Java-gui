55e7e33a0168e89aea4b678c4dcd639ef82e8724
Yolley77 <Ruslan5-5@ya.ru>
2018-10-08 17:03:09 +0300

    Готовая практическая работа №4

--- GUI.java
+++ GUI.java
@@ -6,64 +6,63 @@ import javax.swing.*;
 
 public class GUI extends JFrame {
     
-    JTextField jtf = new JTextField(10);
-    JTextField jtf2 = new JTextField(10);
-    JTextArea jta1 = new JTextArea(10,15);
-    JScrollPane scroll = new JScrollPane(jta1);
-    JButton button = new JButton("Add text");
-    Font fnt = new Font("Times new roman", Font.BOLD,20);
+    JButton milan = new JButton("AC Milan");
+    JButton madrid = new JButton("Real Madrid");
+    JLabel result = new JLabel("Result: 0 x 0");
+    JLabel lastScorer = new JLabel("Last Scorer: N/A");
+    Label winner = new Label("Winner: DRAW");
     
-    JPanel[] panel = new JPanel[14];
+    private int milanScore = 0;
+    private int madridScore = 0;
     
     GUI() {
-        super("джавагуй");
-        setLayout(new FlowLayout());
-        setSize(250, 350);
-        add(new JLabel("FIRST"));
-        add(jtf);
-        jtf.setForeground(Color.YELLOW);
-        jtf.setFont(fnt);
-        add(new JLabel("SECOND"));
-        add(jtf2);
-        add(jta1);
-        add(scroll);
+        super("Соревнование");
+        setLayout(new GridLayout(3, 2));
+        setSize(400, 400);
+        add(milan);
+        add(madrid);
+        add(result);
+        add(lastScorer);
+        add(winner);
         
-        add(button);
-        
-        button.addActionListener(new ActionListener(){
+        milan.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent ae)
             {
-                String txt = JOptionPane.showInputDialog(null,"Insert some text");
-                jta1.append(txt);
+                milanScore++;
+                String txt = "Result: " + milanScore + " x " + madridScore;
+                result.setText(txt);
+                lastScorer.setText("Last Scorer: Milan");
+                if(milanScore > madridScore) {
+                    winner.setText("Winner: Milan");
+                }
+                else if (milanScore < madridScore) {
+                    winner.setText("Winner: Madrid");
+                }
+                else {
+                    winner.setText("Winner: EQUAL SCORE");
+                }
             }
         });
         
-        
-        setLayout(new GridLayout(3,4));
-        for(int i = 0 ; i < panel.length ; i++)
-        {
-            int r = (int) (Math.random() * 255);
-            int b = (int) (Math.random() * 255);
-            int g = (int) (Math.random() * 255);
-            panel[i] = new JPanel();
-            panel[i].setBackground(new Color(r,g,b));
-            add(panel[i]);
-        }
-        panel[4].setLayout(new BorderLayout());
-        panel[4].add(new JButton("one"),BorderLayout.WEST);
-        panel[4].add(new JButton("two"),BorderLayout.EAST);
-        panel[4].add(new JButton("three"),BorderLayout.SOUTH);
-        panel[4].add(new JButton("four"),BorderLayout.NORTH);
-        panel[4].add(new JButton("five"),BorderLayout.CENTER);
-        panel[10].setLayout(new FlowLayout());
-        panel[10].add(new JButton("one"));
-        panel[10].add(new JButton("two"));
-        panel[10].add(new JButton("three"));
-        panel[10].add(new JButton("four"));
-        panel[10].add(new JButton("fve"));
-        setSize(800,500);
-        
-        
+        madrid.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae)
+            {
+                madridScore++;
+                String txt = "Result: " + milanScore + " x " + madridScore;
+                result.setText(txt);
+                lastScorer.setText("Last Scorer: Madrid");
+                if(milanScore > madridScore) {
+                    winner.setText("Winner: Milan");
+                }
+                else if (milanScore < madridScore) {
+                    winner.setText("Winner: Madrid");
+                }
+                else {
+                    winner.setText("Winner: EQUAL SCORE");
+                }
+
+            }
+        });
         setVisible(true);
     }
     
